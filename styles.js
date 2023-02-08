d779858ff983ee5fd076bff161b3f99b6bc56a87
kelvin kipkurui <kkipkurui123@gmail.com>
2023-02-08 16:29:25 +0300

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.querySelector('.hamburger');
+  const list = document.querySelector('.hamburger');
   const menu = document.querySelector('.nav-menu');
   const close = document.querySelector('.fa-solid.fa-xmark');
   const bars = document.querySelector('.fa-solid.fa-bars');
 });
-hamburger.addEventListener('click', () => {
+list.addEventListener('click', () => {
     menu.classList.toggle('show');
     if (menu.classList.contains('show')) {
       bars.style.display = 'none';
