45b4b1997f30aad2751726a2451b6bf4b6a65cdb
unleashed <kkipkurui123@gmail.com>
2023-02-08 14:21:14 +0300

    add styles.js file

---
+++ styles.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.nav-menu');
+  const close = document.querySelector('.fa-solid.fa-xmark');
+  const bars = document.querySelector('.fa-solid.fa-bars');
+});
